1fed34eff0a3098d4d059e62724f1f18cf1d4f7d
Spencer Dixon <spencerlloyddixon@gmail.com>
2016-11-11 13:42:43 +0000

    Removed lorem ipsum from results page

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -5,11 +5,9 @@ class ResultsController < ApplicationController
 
     if params["filter"].present?
       if params["filter"]["gender"].present?
-        @all_other_results = AnswerSet.where("answers->'demographics'->>'gender' = ?", params["filter"]["gender"]
-                             ).pluck(:x_axis_scaled, :y_axis_scaled)
-      end
-
-      if params["filter"]["age"].present?
+        @all_other_results = AnswerSet.where(
+                               "answers->'demographics'->>'gender' = ?", params["filter"]["gender"]
+                             ).reject{ |r| r.id == @results.id }.pluck(:x_axis_scaled, :y_axis_scaled)
       end
     end
 
