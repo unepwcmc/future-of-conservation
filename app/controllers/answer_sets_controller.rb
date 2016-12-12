08daf80ceff9325dd15b930e5a24484c23011e07
Spencer Dixon <spencerlloyddixon@gmail.com>
2016-12-12 14:34:54 +0000

    Save with bang to log errors

--- app/controllers/answer_sets_controller.rb
+++ app/controllers/answer_sets_controller.rb
@@ -5,7 +5,7 @@ class AnswerSetsController < ApplicationController
     @answer_set.ip_address = request.remote_ip
     @answer_set.uuid = SecureRandom.urlsafe_base64
 
-    if @answer_set.save
+    if @answer_set.save!
       redirect_to results_path(@answer_set.uuid), notice: 'Your response was successfully created.'
     else
       redirect_to root_path, notice: 'Something went wrong'
