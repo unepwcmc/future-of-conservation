452b07dc531f6db38941d796972e8dfac7dcfff0
Spencer Dixon <spencerlloyddixon@gmail.com>
2017-01-24 12:27:48 +0000

    Tweaked nav bar and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def active_link_to(name, path)
+    active_class = "active" if current_page?(path)
+    content_tag(:li, class: "nav-item #{active_class}") do
+      link_to(name, path, class: "nav-link")
+    end
+  end
 end
