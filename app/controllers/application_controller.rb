2f97d5df7bb638537389235bbe5f481864556af1
Spencer Dixon <spencerlloyddixon@gmail.com>
2017-01-30 11:25:21 +0000

    Use env variables directly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def authenticate
-    secrets = YAML.load_file("config/secrets.yml")[Rails.env]
-
     authenticate_or_request_with_http_basic('Administration') do |username, password|
-      username == secrets["auth"]["username"] &&
-      password == secrets["auth"]["password"]
+      username == <%= ENV["ADMIN_USERNAME"] %> &&
+      password == <%= ENV["ADMIN_PASSWORD"] %>
     end
   end
 end
