35914b4d5a597010da77c55146054b59ac5b9675
Spencer Dixon <spencerlloyddixon@gmail.com>
2017-03-07 10:00:35 +0000

    Changed use of safe navigator

--- app/models/answer_set.rb
+++ app/models/answer_set.rb
@@ -103,7 +103,8 @@ class AnswerSet < ApplicationRecord
   end
 
   def find_answer_by_key_value(key, value, default=nil)
-    answer = self.answers.dig("questions")&.find {|h| h[key] == value}
+    answer = self.answers.dig("questions")
+    answer = answer&.find {|h| h[key] == value}
     return default if answer.nil?
     answer.empty? ? default : answer["answer_inputted"]
   end
