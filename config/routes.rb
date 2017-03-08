ff71d761391700a5351f85b6a57618834b778d9b
Spencer Dixon <spencerlloyddixon@gmail.com>
2017-03-08 09:43:58 +0000

    Added sidekiq dash to routes in dev and staging only

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   resources :demographic_questions
   get 'static_pages/index'
@@ -14,6 +16,10 @@ Rails.application.routes.draw do
 
   get '/about-the-project', to: 'static_pages#about_the_project', as: 'about_the_project'
   get '/about-the-debate',  to: 'static_pages#about_the_debate',  as: 'about_the_debate'
+
+  if Rails.env.development? || Rails.env.staging?
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
