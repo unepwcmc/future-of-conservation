6b9a24e8fe2aaa84a6a0ef57b30178e5d23873c5
Spencer Dixon <spencerlloyddixon@gmail.com>
2017-03-15 11:07:14 +0000

    Load modules

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module FutureOfConservation
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.active_job.queue_adapter = :sidekiq
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.autoload_paths += %W(#{config.root}/lib/modules)
   end
 end
