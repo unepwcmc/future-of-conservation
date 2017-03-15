c68e3aa6e7e5ed62eaff317de74323c199ccca16
Spencer Dixon <spencerlloyddixon@gmail.com>
2017-03-15 10:38:23 +0000

    Added namespace for sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/2' }
+  config.redis = { url: 'redis://localhost:6379/2', namespace: "future-of-conservation" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/2' }
+  config.redis = { url: 'redis://localhost:6379/2', namespace: "future-of-conservation" }
 end
