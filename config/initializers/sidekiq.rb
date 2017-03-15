7c758d8d447ddedb47567158ea1fb2e5254c8379
Spencer Dixon <spencerlloyddixon@gmail.com>
2017-03-15 10:48:42 +0000

    Removed namespace

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/2', namespace: "future-of-conservation" }
+  config.redis = { url: 'redis://localhost:6379/5'}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/2', namespace: "future-of-conservation" }
+  config.redis = { url: 'redis://localhost:6379/5'}
 end
