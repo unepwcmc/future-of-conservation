22424429eb37517da07db47f068f3bbfaf7202ed
Spencer Dixon <spencerlloyddixon@gmail.com>
2017-03-16 10:41:29 +0000

    Limited results to last 100, added contact email, fixed width of text area field in demographic questions

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -11,7 +11,7 @@ class ResultsController < ApplicationController
 
   def show
     @results = AnswerSet.find_by(uuid: params[:uuid])
-    @all_other_results = AnswerSet.all.reject{ |r| r.id == @results.id }.pluck(:x_axis_scaled, :y_axis_scaled)
+    @all_other_results = AnswerSet.last(100).reject{ |r| r.id == @results.id }.pluck(:x_axis_scaled, :y_axis_scaled)
 
     if params["filter"].present?
       if params["filter"]["gender"].present?
